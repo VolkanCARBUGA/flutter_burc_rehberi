1c686f9d01ba747b6545b5922182b05c1b0258c9
volkan <volkanilkyar@gmail.com>
2023-04-16 14:23:04 +0300

    final codes

--- lib/burc_detay.dart
+++ lib/burc_detay.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
 
 import 'model/burc.dart';
 
@@ -11,24 +12,34 @@ class BurcDetay extends StatefulWidget {
 }
 
 class _BurcDetayState extends State<BurcDetay> {
+  Color appBarRengi = Colors.pink;
+  late PaletteGenerator paletGenerator;
+  @override
+  void initState() {
+    super.initState();
+    appBarRenkBul();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            backgroundColor: appBarRengi,
             title: Text(widget.secilenBurc.burcAdi.toUpperCase()),
             expandedHeight: 250,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
-              background: Image.asset("images/" + widget.secilenBurc.burcBuyukResim),
+              background:
+                  Image.asset("images/" + widget.secilenBurc.burcBuyukResim),
             ),
           ),
           SliverToBoxAdapter(
             child: Center(
               child: SingleChildScrollView(
                 child: Card(
-                  color: Colors.blueAccent,
+                  color:appBarRengi,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(0)),
                   child: Column(
@@ -55,5 +66,14 @@ class _BurcDetayState extends State<BurcDetay> {
         ],
       ),
     );
+  }
+
+  void appBarRenkBul() async {
+    paletGenerator = await PaletteGenerator.fromImageProvider(
+        AssetImage("images/${widget.secilenBurc.burcBuyukResim}"));
+    appBarRengi = paletGenerator.dominantColor!.color;
+    setState(() {
+      
+    });
   }
 }
