83c168584c4a93aa0c62d6e763060311c3b2571b
volkan <volkanilkyar@gmail.com>
2023-04-16 11:26:42 +0300

    ilk kodlarım

--- lib/burc_listesi.dart
+++ lib/burc_listesi.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_burc_rehberi/model/burc.dart';
+import 'package:flutter_burc_rehberi/strings.dart';
 
 class BurcListesi extends StatefulWidget {
- late List<Burc> tumBurclar;
-  BurcListesi({super.key}) {
-    tumBurclar = [];
+  late List<Burc> tumBurclar;
+  BurcListesi() {
+    tumBurclar = veriKaynagiHazirla();
   }
 
   @override
   State<BurcListesi> createState() => _BurcListesiState();
+}
+
+List<Burc> veriKaynagiHazirla() {
+  List<Burc> gecici = [];
+  for (var i = 0; i < 12; i++) {
+    var burcAdi = Strings.BURC_ADLARI[i];
+    var burcTarihi = Strings.BURC_TARIHLERI[i];
+    var burcDetay = Strings.BURC_GENEL_OZELLIKLERI[i];
+    //burç resimlerini oluşturmak için
+    var burcKucukResim = Strings.BURC_ADLARI[i].toLowerCase() + "${i + 1}.png";
+    var burcBuyukResim = Strings.BURC_ADLARI[i].toLowerCase()+"_buyuk" + "${i + 1}.png";
+    Burc eklenecekBurc = Burc(burcAdi,burcTarihi,burcDetay,burcKucukResim,burcBuyukResim);
+    gecici.add(eklenecekBurc);
+  }
+  return gecici;
 }
 
 class _BurcListesiState extends State<BurcListesi> {
@@ -20,9 +36,8 @@ class _BurcListesiState extends State<BurcListesi> {
       ),
       body: Container(
         child: Center(
-          child: Text("Burçlar Listesi"),
-        ),
+          child: ListView.builder(itemBuilder: (context, index) => ,itemCount: tumBurclar.lenght,)
       ),
-    );
+    ));
   }
 }
